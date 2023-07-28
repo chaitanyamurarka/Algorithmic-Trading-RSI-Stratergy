449ee8d20420d6a4feef655288eabeca18cbd1ff
Chaitanya Murarka <murarkacj@@gmail.com>
2023-07-28 12:51:21 +0530

    STOP Exe Updated

--- app.py
+++ app.py
@@ -44,86 +44,86 @@ if start_button:
     
     while True:
         
-        if stop_button:
-            st.write('Program Stopped Successfully')
-            break
-        
-        if data.index[-1].date() == pd.Timestamp.now().date() and data.index[-1].hour() == pd.Timestamp.now().hour() and data.index[-1].second() == pd.Timestamp.now().second():
-        
-            # Calculate RSI for the given period (14 in this case)
-            data['rsi'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
-
-            if previous_candle['Close'] > previous_candle['Open']:
-                st.write("Previous candle was bullish.")
-                pre_bull_or_bear = 'bull'
-                if data.iloc[-2]['rsi'] < oversold_rsi_threshold:
-                    # If RSI is less than the oversold threshold, Buy 1 lot / stock at the Close price of the previous candle.
-                    place_trade("Buy", lot_size, previous_candle['Close'])
-                else:
-                    st.write(f"RSI is {data.iloc[-2]['rsi']} not less than oversold range of {oversold_rsi_threshold} . Skipping trade.")
-            else:
-                st.write("Previous candle was bearish.")
-                pre_bull_or_bear = 'bear'
-                if data.iloc[-2]['rsi'] > overbought_rsi_threshold:
-                    # If RSI is greater than the overbought threshold, Sell 1 lot / stock at the Close price of the previous candle.
-                    place_trade("Sell", lot_size, previous_candle['Close'])
-                else:
-                    st.write(f"RSI is {data.iloc[-2]['rsi']} not more than overbought range of {overbought_rsi_threshold}. Skipping trade.")
-
-            # Wait until the end of the current minute (59th second of the minute)
-            current_time = pd.Timestamp.now().time()
-            seconds_to_next_minute = 57 - current_time.second
-            time.sleep(seconds_to_next_minute)
-            cancel_pending()
-            data = yf.download(tickers=stock_name, period='1d', interval='1m')
-            current_candle = data.iloc[-1]
-            st.write(current_candle)
-
-            if pre_bull_or_bear == 'bull':
-                # At the end of the candle, check if the position is profitable
-                if previous_candle['Close'] < current_candle['Close']:
-                    # The position was profitable, reset the consecutive losses counter
-                    st.write("Position was profitable. Keeping lot size at 1.")
-                    consecutive_losses = 0
-                else:
-                    # The position was a loss
-                    consecutive_losses += 1
-                    if consecutive_losses >= martingale_limit:
-                        st.write(f"Reached Martingale Limit of {martingale_limit} consecutive losses. Stopping.")
-                        break
-                    else:
-                        # Multiply lot size by profit_factor
-                        st.write("Position was a loss. Multiplying lot size by 2.")
-                        lot_size *= profit_factor
-            else:
-                # At the end of the candle, check if the position is profitable
-                if previous_candle['Close'] > current_candle['Close']:
-                    # The position was profitable, reset the consecutive losses counter
-                    st.write("Position was profitable. Keeping lot size at 1.")
-                    consecutive_losses = 0
-                else:
-                    # The position was a loss
-                    consecutive_losses += 1
-                    if consecutive_losses >= martingale_limit:
-                        st.write(f"Reached Martingale Limit of {martingale_limit} consecutive losses. Stopping.")
-                        break
-                    else:
-                        # Multiply lot size by profit_factor
-                        st.write("Position was a loss. Multiplying lot size by 2.")
-                        lot_size *= profit_factor
+        while not stop_button:
+         
+            if data.index[-1].date() == pd.Timestamp.now().date() and data.index[-1].hour == pd.Timestamp.now().hour and data.index[-1].minute == pd.Timestamp.now().minute:
             
-            # Wait until the start of the next minute
-            current_time = pd.Timestamp.now().time()
-            seconds_to_next_minute = 62 - current_time.second
-            time.sleep(seconds_to_next_minute)
+                # Calculate RSI for the given period (14 in this case)
+                data['rsi'] = ta.momentum.RSIIndicator(data['Close'], window=14).rsi()
 
-            # Interval required 1 minute
-            data = yf.download(tickers=stock_name, period='1d', interval='1m')
+                if previous_candle['Close'] > previous_candle['Open']:
+                    st.write("Previous candle was bullish.")
+                    pre_bull_or_bear = 'bull'
+                    if data.iloc[-2]['rsi'] < oversold_rsi_threshold:
+                        # If RSI is less than the oversold threshold, Buy 1 lot / stock at the Close price of the previous candle.
+                        place_trade("Buy", lot_size, previous_candle['Close'])
+                    else:
+                        st.write(f"RSI is {data.iloc[-2]['rsi']} not less than oversold range of {oversold_rsi_threshold} . Skipping trade.")
+                else:
+                    st.write("Previous candle was bearish.")
+                    pre_bull_or_bear = 'bear'
+                    if data.iloc[-2]['rsi'] > overbought_rsi_threshold:
+                        # If RSI is greater than the overbought threshold, Sell 1 lot / stock at the Close price of the previous candle.
+                        place_trade("Sell", lot_size, previous_candle['Close'])
+                    else:
+                        st.write(f"RSI is {data.iloc[-2]['rsi']} not more than overbought range of {overbought_rsi_threshold}. Skipping trade.")
 
-            # Assuming 'data' contains the historical stock data fetched using yfinance
-            previous_candle = data.iloc[-2]  # Get the row for the previous candle
-            st.write("Previous Candle",previous_candle)
+                # Wait until the end of the current minute (59th second of the minute)
+                current_time = pd.Timestamp.now().time()
+                seconds_to_next_minute = 57 - current_time.second
+                time.sleep(seconds_to_next_minute)
+                cancel_pending()
+                data = yf.download(tickers=stock_name, period='1d', interval='1m')
+                current_candle = data.iloc[-1]
+                st.write(current_candle)
+
+                if pre_bull_or_bear == 'bull':
+                    # At the end of the candle, check if the position is profitable
+                    if previous_candle['Close'] < current_candle['Close']:
+                        # The position was profitable, reset the consecutive losses counter
+                        st.write("Position was profitable. Keeping lot size at 1.")
+                        consecutive_losses = 0
+                    else:
+                        # The position was a loss
+                        consecutive_losses += 1
+                        if consecutive_losses >= martingale_limit:
+                            st.write(f"Reached Martingale Limit of {martingale_limit} consecutive losses. Stopping.")
+                            break
+                        else:
+                            # Multiply lot size by profit_factor
+                            st.write("Position was a loss. Multiplying lot size by 2.")
+                            lot_size *= profit_factor
+                else:
+                    # At the end of the candle, check if the position is profitable
+                    if previous_candle['Close'] > current_candle['Close']:
+                        # The position was profitable, reset the consecutive losses counter
+                        st.write("Position was profitable. Keeping lot size at 1.")
+                        consecutive_losses = 0
+                    else:
+                        # The position was a loss
+                        consecutive_losses += 1
+                        if consecutive_losses >= martingale_limit:
+                            st.write(f"Reached Martingale Limit of {martingale_limit} consecutive losses. Stopping.")
+                            break
+                        else:
+                            # Multiply lot size by profit_factor
+                            st.write("Position was a loss. Multiplying lot size by 2.")
+                            lot_size *= profit_factor
+                
+                # Wait until the start of the next minute
+                current_time = pd.Timestamp.now().time()
+                seconds_to_next_minute = 62 - current_time.second
+                time.sleep(seconds_to_next_minute)
+
+                # Interval required 1 minute
+                data = yf.download(tickers=stock_name, period='1d', interval='1m')
+
+                # Assuming 'data' contains the historical stock data fetched using yfinance
+                previous_candle = data.iloc[-2]  # Get the row for the previous candle
+            
+            else:
+                st.write('Real Time Data from Stock Market not Avaible ,Pls Try after sometime')
+                break
         
-        else:
-            st.write('Real Time Data from Stock Market not Avaible ,Pls Try after sometime')
-            break
+        st.write("Program Stopped Successfully")
+        break
